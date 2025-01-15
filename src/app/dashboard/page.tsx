29296473a5d96b2a70255ef2e1e31fce8cebb52e
Felipe <ti@vcaconstrutora.com.br>
2025-01-15 10:29:06 -0300

    adiciona texto em dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <section className="w-full h-full flex items-center justify-center"></section>
+    <section className="w-full h-full flex items-center justify-center">
+      {"Para visualizar as soluções disponíveis, acesse a aba 'Setores'."}
+    </section>
   )
 }
