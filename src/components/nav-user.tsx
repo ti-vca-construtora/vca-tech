80706d0e91626f8b277117ff9fd394c3bf3d1b94
Felipe <ti@vcaconstrutora.com.br>
2025-01-27 14:20:56 -0300

    fix: ajusta lint

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,14 +1,8 @@
-"use client"
+'use client'
 
-import {
-  LogOut,
-  User2,
-} from "lucide-react"
+import { LogOut, User2 } from 'lucide-react'
 
-import {
-  Avatar,
-
-} from "@/components/ui/avatar"
+import { Avatar } from '@/components/ui/avatar'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -16,14 +10,14 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from '@/components/ui/dropdown-menu'
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from "@/components/ui/sidebar"
-import { CaretSortIcon } from "@radix-ui/react-icons"
+} from '@/components/ui/sidebar'
+import { CaretSortIcon } from '@radix-ui/react-icons'
 
 type NavUserProps = {
   user: {
@@ -33,10 +27,7 @@ type NavUserProps = {
   logout: () => void
 }
 
-export function NavUser({
-  user,
-  logout
-}: NavUserProps) {
+export function NavUser({ user, logout }: NavUserProps) {
   const { isMobile } = useSidebar()
 
   return (
@@ -60,7 +51,7 @@ export function NavUser({
           </DropdownMenuTrigger>
           <DropdownMenuContent
             className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
+            side={isMobile ? 'bottom' : 'right'}
             align="end"
             sideOffset={4}
           >
