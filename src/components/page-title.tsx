f0913b1a91ed051eadbddc7a3c170aca362d0d97
Felipe <ti@vcaconstrutora.com.br>
2025-01-27 15:13:44 -0300

    fix: solucao do suspense boundary

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export function PageTitle() {
   const searchParams = useSearchParams()
+  const [pageTitle, setPageTitle] = useState<string | null>(null)
 
-  const pageTitle = searchParams.get('title')
+  useEffect(() => {
+    const title = searchParams.get('title')
+    setPageTitle(title)
+  }, [searchParams])
 
   return (
     <div className="flex items-center shadow-md bg-neutral-100 p-4 rounded-lg">
