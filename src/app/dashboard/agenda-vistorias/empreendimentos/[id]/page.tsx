1a5f7d1367b22bb1be60a6a5c776bc54340a8543
SilasPires <pro.silaspires@gmail.com>
2025-03-18 17:39:49 -0300

    feat: cadastrar empreendimentos

--- src/app/dashboard/agenda-vistorias/empreendimentos/[id]/page.tsx
+++ src/app/dashboard/agenda-vistorias/empreendimentos/[id]/page.tsx
@@ -13,46 +13,101 @@ import {
 } from '@/components/ui/select'
 import { Trash } from 'lucide-react'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-const CadastrarEmpreendimento = () => {
+const EditarEmpreendimento = () => {
+  const idParam = useParams()
+  const route = useRouter()
+
   const [empreendimentoNome, setEmpreendimentoNome] = useState('')
-  const [isActive, setIsActive] = useState<string | boolean>('')
+  const [isActive, setIsActive] = useState<string>('')
 
-  const createDevelopment = async () => {
+  const patchDevelopment = async () => {
     if (empreendimentoNome === '' || isActive === '') {
       window.alert('Campos obrigatórios não preenchidos!')
       return
     }
-    if (isActive === 'false') {
-      setIsActive(false)
-    } else {
-      setIsActive(true)
-    }
 
-    const response = await fetch('/api/vistorias/empreendimentos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: empreendimentoNome.toUpperCase(),
-        isActive,
-      }),
-    })
+    const booleanActive = isActive === 'true'
+
+    const response = await fetch(
+      `/api/vistorias/empreendimentos?id=${idParam.id}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: empreendimentoNome.toUpperCase(),
+          isActive: booleanActive,
+        }),
+        // eslint-disable-next-line prettier/prettier
+      }
+    )
 
     if (!response.ok) {
-      console.error('Erro ao criar o empreendimento')
+      console.error('Erro ao editar o empreendimento')
       return
     }
 
     const data = await response.json()
-    console.log('Empreendimento criado:', data)
+    console.log('Empreendimento editado:', data)
+    route.push('/dashboard/agenda-vistorias/empreendimentos')
+  }
+
+  const deleteDevelopment = async () => {
+    const response = await fetch(
+      `/api/vistorias/empreendimentos?id=${idParam.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // eslint-disable-next-line prettier/prettier
+      }
+    )
+
+    if (!response.ok) {
+      console.error('Erro ao excluir o empreendimento')
+      return
+    }
+
+    const data = await response.json()
+    console.log('Empreendimento excluído:', data)
+    route.push('/dashboard/agenda-vistorias/empreendimentos')
   }
 
   const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmpreendimentoNome(e.target.value)
   }
+
+  const getEmpreendimento = async () => {
+    const response = await fetch(
+      `/api/vistorias/empreendimentos?id=${idParam.id}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // eslint-disable-next-line prettier/prettier
+      }
+    )
+
+    if (!response.ok) {
+      console.error('Erro ao carregar empreendimentos')
+      return
+    }
+
+    const data = await response.json()
+    setEmpreendimentoNome(data.data.name)
+    setIsActive(data.data.isActive ? 'true' : 'false')
+  }
+
+  useEffect(() => {
+    getEmpreendimento()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <section className="flex flex-col items-start gap-6 w-full h-full p-6">
@@ -60,6 +115,7 @@ const CadastrarEmpreendimento = () => {
         <div className="grid w-full items-center gap-1.5">
           <Label htmlFor="empreendimento">Empreendimento</Label>
           <Input
+            value={empreendimentoNome}
             type="text"
             id="empreendimento"
             placeholder="Nome"
@@ -68,7 +124,7 @@ const CadastrarEmpreendimento = () => {
         </div>
         <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
           <Label htmlFor="status">Status</Label>
-          <Select onValueChange={setIsActive}>
+          <Select onValueChange={setIsActive} value={isActive}>
             <SelectTrigger id="status" className="w-[260px] cursor-pointer">
               <SelectValue placeholder="Selecione" />
             </SelectTrigger>
@@ -84,6 +140,7 @@ const CadastrarEmpreendimento = () => {
         </div>
         <div className="grid w-full items-center gap-1.5 mt-6">
           <Trash
+            onClick={deleteDevelopment}
             className="bg-red-600 text-neutral-100 p-1 rounded cursor-pointer hover:scale-110 duration-100"
             width={30}
             height={30}
@@ -94,7 +151,7 @@ const CadastrarEmpreendimento = () => {
             <Button variant="destructive">Cancelar</Button>
           </Link>
           <Button
-            onClick={createDevelopment}
+            onClick={patchDevelopment}
             variant="default"
             className="bg-azul-claro-vca hover:bg-verde-vca"
           >
@@ -105,4 +162,4 @@ const CadastrarEmpreendimento = () => {
     </section>
   )
 }
-export default CadastrarEmpreendimento
+export default EditarEmpreendimento
--- src/app/dashboard/agenda-vistorias/empreendimentos/page.tsx
+++ src/app/dashboard/agenda-vistorias/empreendimentos/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Card,
   CardContent,
@@ -6,6 +8,22 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
   Table,
   TableBody,
   TableCell,
@@ -13,53 +31,133 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
 import { MoreHorizontal } from 'lucide-react'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+type Empreendimento = {
+  id: string
+  name: string
+  isActive: boolean
+}
 
 export default function Empreendimentos() {
+  const [empreendimentos, setEmpreendimentos] = useState<Empreendimento[]>([])
+  const [selectedEmpreendimento, setSelectedEmpreendimento] =
+    useState<Empreendimento>({
+      id: '',
+      name: '',
+      isActive: true,
+    })
+
+  const getEmpreendimentos = async () => {
+    const response = await fetch(
+      '/api/vistorias/empreendimentos?page=1&pageSize=20',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // eslint-disable-next-line prettier/prettier
+      }
+    )
+
+    if (!response.ok) {
+      console.error('Erro ao carregar empreendimentos')
+      return
+    }
+
+    const data = await response.json()
+    setEmpreendimentos(data.data)
+    console.log('Empreendimentos lsitados:', data.data)
+  }
+
+  useEffect(() => {
+    getEmpreendimentos()
+  }, [])
+
   return (
     <section className="flex flex-col items-start gap-6 w-full h-full p-6">
-      <Card className="w-full">
-        <CardHeader className="flex justify-between flex-row">
-          <div>
-            <CardTitle>Empreendimentos</CardTitle>
-            <CardDescription>Gerenciamento de empreendimentos.</CardDescription>
-          </div>
-          <div>
-            <Link href="/dashboard/agenda-vistorias/empreendimentos/cadastrar?title=Cadastrar+empreendimento">
-              <button className="px-4 py-1 rounded-md bg-azul-claro-vca text-white">
-                Cadastrar empreendimento
-              </button>
-            </Link>
-          </div>
-        </CardHeader>
-        <CardContent>
-          <Table className="p-2 rounded bg-white">
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[350px]">Empreendimento</TableHead>
-                <TableHead className="w-44">Status</TableHead>
-                <TableHead className="w-16 text-center"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow className="bg-neutral-50">
-                <TableCell className="flex flex-col w-fit">
-                  <span className="font-semibold">
-                    DONNA OLÍVIA UNIVERSIDADE
-                  </span>
-                </TableCell>
-                <TableCell className="text-start">
-                  <span className="text-sm">Ativo</span>
-                </TableCell>
-                <TableCell className="">
-                  <MoreHorizontal />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
+      <Dialog>
+        <Card className="w-full">
+          <CardHeader className="flex justify-between flex-row">
+            <div>
+              <CardTitle>Empreendimentos</CardTitle>
+              <CardDescription>
+                Gerenciamento de empreendimentos.
+              </CardDescription>
+            </div>
+            <div>
+              <Link href="/dashboard/agenda-vistorias/empreendimentos/cadastrar?title=Cadastrar+empreendimento">
+                <button className="px-4 py-1 rounded-md bg-azul-claro-vca text-white">
+                  Cadastrar empreendimento
+                </button>
+              </Link>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <Table className="p-2 rounded bg-white">
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[350px]">Empreendimento</TableHead>
+                  <TableHead className="w-44">Status</TableHead>
+                  <TableHead className="w-16 text-center"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {empreendimentos.map((index) => (
+                  <TableRow key={index.id} className="bg-neutral-50">
+                    <TableCell className="flex flex-col w-fit">
+                      <span className="font-semibold">{index.name}</span>
+                    </TableCell>
+                    <TableCell className="text-start">
+                      <span className="text-sm">
+                        {index.isActive ? 'Ativo' : 'Inativo'}
+                      </span>
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger>
+                          <MoreHorizontal />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                          <DropdownMenuLabel>Opções</DropdownMenuLabel>
+                          <DropdownMenuSeparator />
+                          <Link
+                            href={`/dashboard/agenda-vistorias/empreendimentos/${index.id}`}
+                          >
+                            <DropdownMenuItem className="cursor-pointer">
+                              Editar
+                            </DropdownMenuItem>
+                          </Link>
+                          <DialogTrigger className="flex w-full">
+                            <DropdownMenuItem
+                              onClick={() => setSelectedEmpreendimento(index)}
+                              className="cursor-pointer flex w-full"
+                            >
+                              Unidades
+                            </DropdownMenuItem>
+                          </DialogTrigger>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Configurar Unidades</DialogTitle>
+            <DialogDescription>
+              {selectedEmpreendimento.id}
+              {selectedEmpreendimento.name}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </section>
   )
 }
