b29473038fbc61de2c90f52c111f137a1a0ee8dd
maurodk <144278242+ti-vca-construtora@users.noreply.github.com>
2025-12-16 11:32:31 -0300

    Update simulador-form.tsx

--- src/app/dashboard/(solucoes)/simulador-financiamento-caixa/_components/simulador-form.tsx
+++ src/app/dashboard/(solucoes)/simulador-financiamento-caixa/_components/simulador-form.tsx
@@ -210,6 +210,7 @@ export function SimuladorForm() {
         possuiTresAnosFGTS,
         jaBeneficiadoSubsidio,
         possuiDependentes,
+        quantidadeParticipantes,
         sistemaAmortizacao,
         participantes: participantesAjustados,
       };
