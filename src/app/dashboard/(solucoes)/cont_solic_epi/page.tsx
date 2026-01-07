7f5b255d8700879e597687f7991dd08f2f6ec88b
maurodk <ti@vcaconstrutora.com.br>
2026-01-07 09:39:28 -0300

    Update page.tsx

--- src/app/dashboard/(solucoes)/cont_solic_epi/page.tsx
+++ src/app/dashboard/(solucoes)/cont_solic_epi/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-export default function ContSolicEpi() {
+import { RouteGuard } from "@/components/route-guard";
+import { ContSolicEpi } from "./_components/cont-solic-epi";
+
+export default function ContSolicEpiPage() {
   return (
     <RouteGuard requiredArea="sesmt" requiredPermission="epi">
       <div className="w-full h-full flex flex-col gap-6 p-6">
